f1cabf3b09e1aa4bd5ade7594092b785155c18fe
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2022-10-04 16:54:47 +0300

    autorefresh

--- client/app/components/screens/Profile/Post/Post.tsx
+++ client/app/components/screens/Profile/Post/Post.tsx
@@ -20,18 +20,37 @@ const Post: FC<IPost> = ({ text, image, likes, comments, date, _id }) => {
   const {store} = useContext(Context)
   const router = useRouter()
   const postId = _id;
+  const [localUser, setLocalUser] = useState<IPost>({text: text, image: image, likes: likes, comments: comments, date: date, _id: _id})
+  var prevLocalUser = localUser
+  // const [likess, setLikes] = useState<number>(likes)
   
   const changeLikes = async (): Promise<void> => {
     const userId = router.query.id
     if (!isLikeActive){
       setIsLikeActive(true)
       await store.addLike(userId, _id)
-      window.location.reload()
+      if (router.asPath[1] === 'p'){
+        router.push(`/profile/${userId}`)
+      }
+      if (router.asPath[1] === 'u'){
+        await router.push(`/users/${userId}`)
+        prevLocalUser.likes = prevLocalUser.likes + 1
+        setLocalUser(prevLocalUser)
+        router.push(`/users/${userId}`)
+      }
     }
     if (isLikeActive){
       setIsLikeActive(false)
       await store.deleteLike(userId, _id)
-      window.location.reload()
+      if (router.asPath[1] === 'p'){
+        router.push(`/profile/${userId}`)
+      }
+      if (router.asPath[1] === 'u'){
+        await router.push(`/users/${userId}`)
+        prevLocalUser.likes = prevLocalUser.likes - 1
+        setLocalUser(prevLocalUser)
+        router.push(`/users/${userId}`)
+      }
     }
   }
 
@@ -51,7 +70,15 @@ const Post: FC<IPost> = ({ text, image, likes, comments, date, _id }) => {
   const handleComment = async (): Promise<void> => {
     if (commentValue !== '') {
       await store.addComment(router.query.id, store.user.id, commentValue, _id)
-      window.location.reload()
+      if (router.asPath[1] === 'p'){
+        await router.push(`/profile/${store.user.id}`)
+      }
+      if (router.asPath[1] === 'u'){
+        await router.push(`/users/${router.query.id}`)
+        prevLocalUser.comments = 
+        setLocalUser(prevLocalUser)
+        router.push(`/users/${router.query.id}`)
+      }
       setCommentValue('')
     } else{
       setEmptyComment(true)
@@ -92,7 +119,7 @@ const Post: FC<IPost> = ({ text, image, likes, comments, date, _id }) => {
               <svg className={isLikeActive ? `${styles['likes-img']} ${styles.active}` : `${styles['likes-img']}`} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" >
                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
               </svg>
-              <h2 className={styles['likes-num']}>{likes}</h2>
+              <h2 className={styles['likes-num']}>{localUser.likes}</h2>
             </div>
             <div className={styles['post-comments']}>
               <FaRegCommentAlt className={isComment ? `${styles['comments-img']} ${styles.active}` : styles['comments-img']}  onClick={() => setIsComment(!isComment)}/>
